7314825814da672b250159fdbcc8761a4e958330
wissalcodes <lw_messikh@esi.dz>
2024-03-04 12:49:28 +0100

    fixed folder structure

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
-import AboutUs from "./components/landing_page/landing_page/AboutUs";
-import Solutions from "./components/landing_page/landing_page/Solutions";
-import FAQ from "./components/landing_page/landing_page/FAQ";
+import AboutUs from "../components/landing_page/landing_page/AboutUs";
+import Solutions from "../components/landing_page/landing_page/Solutions";
+import FAQ from "../components/landing_page/landing_page/FAQ";
 export default function Home() {
   return (
     <div className="font-lora flex flex-col items-center justify-center w-full h-full px-[40px] overflow-x-hidden">
